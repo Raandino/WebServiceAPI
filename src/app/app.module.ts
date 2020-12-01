b4baf497b6c158f29b8616e552b92f89c9111c0f
Roberto Sanchez <sanchez.roberto0810@gmail.com>
2020-12-01 17:31:01 -0600

    Componiendo el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ToastrModule } from 'ngx-toastr';
 import { CommonModule } from '@angular/common';
 
-
 import { AppRoutingModule } from './Views/app/app-routing.module';
 import { AppComponent } from './Views/app/app.component';
-import { ClientComponent }     from './Views/clients/client/client.component';
-import { ClientsComponent }    from './Views/clients/clients.component';
+import { ClientComponent } from './Views/clients/client/client.component';
+import { ClientsComponent } from './Views/clients/clients.component';
 import { ClientListComponent } from './Views/clients/client-list/client-list.component';
-
 
 import { ClientService } from './Controllers/Client/client.service';
 import { HomeComponent } from './Views/home/home.component';
@@ -29,24 +27,21 @@ import { InventoryComponent } from './Views/inventory/inventory.component';
 import { UsersComponent } from './Views/users/users.component';
 import { OrdersComponent } from './Views/orders/orders.component';
 import { ProductsComponent } from './Views/products/products.component';
-import { InventoryListComponent } from './Views/Inventory/inventory-list/inventory-list.component';
+import { InventoryListComponent } from './Views/inventory/inventory-list/inventory-list.component';
 import { ProductComponent } from './Views/products/product/product.component';
 import { ProductListComponent } from './Views/products/product-list/product-list.component';
-import { UserComponent } from './views/users/user/user.component';
-import { UserListComponent } from './views/users/user-list/user-list.component';
-import { OrderComponent } from './views/orders/order/order.component';
-import { OrderListComponent } from './views/orders/order-list/order-list.component';
-import { InventoryItemComponent } from './views/Inventory/inventory-item/inventory-item.component';
-
+import { UserComponent } from './Views/users/user/user.component';
+import { UserListComponent } from './Views/users/user-list/user-list.component';
+import { OrderComponent } from './Views/orders/order/order.component';
+import { OrderListComponent } from './Views/orders/order-list/order-list.component';
+import { InventoryItemComponent } from './Views/inventory/inventory-item/inventory-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
-   
-   
+
     TokenComponent,
-   
+
     InventoryComponent,
     UsersComponent,
     OrdersComponent,
@@ -68,13 +63,13 @@ import { InventoryItemComponent } from './views/Inventory/inventory-item/invento
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    BrowserModule, 
+    BrowserModule,
     RouterModule,
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    MDBBootstrapModule
+    MDBBootstrapModule,
   ],
   providers: [ClientService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
