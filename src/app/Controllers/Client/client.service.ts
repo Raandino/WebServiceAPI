83664b9987352f041a0a718d7f2abc54795c1ccc
RODOLFO ANDINO <aandino186@gmail.com>
2020-12-02 22:44:06 -0600

    Cliente crud sin auth

--- src/app/Controllers/Client/client.service.ts
+++ src/app/Controllers/Client/client.service.ts
@@ -17,18 +17,32 @@ export class ClientService {
               private TokenService : TokenService) { }
 
 
-  postClient (form : Client){
-    return this.http.post(this.rootURL+'/clients ',this.formData,this.getHeaders());
+  // postClient (form : Client){
+  //   return this.http.post(this.rootURL+'/clients ',this.formData,this.getHeaders());
+    
+  // }
+
+  // putClient (form : Client){
+  //   return this.http.put(this.rootURL+'/clients/ '+form.client_id,this.formData,this.getHeaders());
+  // }
+
+  // deleteClient(id :number){
+  //   return this.http.delete(this.rootURL+'/clients/ '+id,this.getHeaders());
+  // }
+
+    postClient (form : Client){
+    return this.http.post(this.rootURL+'/clients ',this.formData);
     
   }
 
   putClient (form : Client){
-    return this.http.put(this.rootURL+'/clients/ '+form.client_id,this.formData,this.getHeaders());
+    return this.http.put(this.rootURL+'/clients/ '+form.client_id,this.formData);
   }
 
   deleteClient(id :number){
-    return this.http.delete(this.rootURL+'/clients/ '+id,this.getHeaders());
+    return this.http.delete(this.rootURL+'/clients/ '+id);
   }
+
 
 
   refreshList (){
