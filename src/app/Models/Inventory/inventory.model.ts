999e787a92425a9b97b6f36e3b033ee3894b5237
RODOLFO ANDINO <aandino186@gmail.com>
2020-12-03 23:19:37 -0600

    modelos completos

--- src/app/Models/Inventory/inventory.model.ts
+++ src/app/Models/Inventory/inventory.model.ts
@@ -1,2 +1,10 @@
 export class Inventory {
+    inventory_id?:number
+    product_id:number
+    user_id:number
+    quantity:number
+    measure_id: number
+    date_created: Date
+    date_modify: Date
+    price: number
 }
--- src/app/Models/User/user.model.ts
+++ src/app/Models/User/user.model.ts
@@ -1,2 +1,10 @@
 export class User {
+    user_id?:number
+    name:string
+    phone:number
+    email: string
+    username:string
+    password_user:string
+    type_user_id:number
+    date_created:Date
 }
