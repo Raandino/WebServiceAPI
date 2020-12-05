8048f6f67cec0feb1d810cd24263a8bc4d435bea
RODOLFO ANDINO <aandino186@gmail.com>
2020-12-04 22:52:24 -0600

    Inventory-list added

--- src/app/Views/inventory/inventory-list/inventory-list.component.ts
+++ src/app/Views/inventory/inventory-list/inventory-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import {InventoryService} from '../../../Controllers/Inventory/inventory.service'
+import {Inventory} from '../../../Models/Inventory/inventory.model'
 
 @Component({
   selector: 'app-inventory-list',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service : InventoryService,
+    private toastr : ToastrService) { }
+
+
+    InventorytList:any=[];
+    formatinventoryList:any=[];
 
   ngOnInit(): void {
+    this.service.refreshList();
+    
+  }
+
+  
+  populateForm(cl: Inventory){
+    this.service.formData = Object.assign({},cl);
+    
+  }
+
+
+  onDelete(id: number){
+    if(confirm('Are you sure to delete this record?')){
+      this.service.delete(id).subscribe(res =>{
+        this.service.refreshList();
+        this.toastr.warning('Deleted Successfully','Client Register');
+      })  
+    }
+    
   }
 
 }
